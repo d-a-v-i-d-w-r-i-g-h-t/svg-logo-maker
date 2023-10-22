cf5faa9a0bdff23db19ce6fc024d0aae8787d4eb
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-22 15:36:27 -0700

    Add pseudocode

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+
+// Pseudocode
+
+//  1. Prompt user for logo text
+//     a. response must have 1-3 characters
+
+//  2. Prompt user for text color
+//     a. color keyword (check against list of color keywords?)
+//     b. hexadecimal number
+
+//  3. Prompt user for shape
+//     a. type: list
+//     b. options: circle, triangle, square
+
+//  4. Prompt user for shape color
+//     a. color keyword (check against list of color keywords?)
+//     b. hexadecimal number
+
+//  5. Create an SVG file
+//     a. proper shape, shape color
+//     b. text, text color
+//     c. size 300x200 pixels
+
+//  6. Save SVG file
+//     a. if necessary, create the folder 'output'
+//     b. save the file as logo.svg in the output folder
+
+//  7. Display success message
+//     a. 'Generated logo.svg' printed to the command line
